29dd7f5b89c943ca8682351af3f3a98feb037d05
Francesca-Lisi <francescalisi2143@gmail.com>
2025-03-27 22:29:54 +0100

    feat: snack 3 and 4 completed

--- Snack-3/script.js
+++ Snack-3/script.js
@@ -1,3 +1,13 @@
 // Crea una funzione eseguiOperazione
 
 // Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
+function somma(numA, numB) {
+    console.log(numA + numB)
+}
+
+
+function eseguiOperazione(numA, numB, callback) {
+    callback(numA, numB)
+}
+
+eseguiOperazione(10, 6, somma)
--- Snack-4/script.js
+++ Snack-4/script.js
@@ -1,1 +1,7 @@
 // Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
+
+function creaTimer(time) {
+    setTimeout(() => console.log('Tempo Scaduto!'), time)
+}
+
+creaTimer(3000)
